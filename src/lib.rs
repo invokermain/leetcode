800f4022bc1f6d427a89deeb2bb3438e00db8d23
Tim OSullivan <44192816+invokermain@users.noreply.github.com>
2022-12-29 18:43:29 +0000

    feat (347): complete

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ mod problem_217;
 mod problem_2289;
 mod problem_234;
 mod problem_242;
+mod problem_347;
 mod problem_383;
 mod problem_412;
 mod problem_49;
---
+++ src/problem_347.rs
@@ -0,0 +1,34 @@
+use std::collections::HashMap;
+
+// This algorithm uses a hashmap to count values.
+// It is scored at 4ms (75%) and 2.4MB (85%).
+fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
+    let values_map = nums.iter().fold(HashMap::<i32, u16>::new(), |mut map, x| {
+        *map.entry(*x).or_default() += 1;
+        map
+    });
+    let mut values: Vec<(i32, u16)> = values_map.into_iter().collect();
+    values.sort_by_key(|x| x.1);
+    values.reverse();
+    values[..k as usize].iter().map(|x| x.0 as i32).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::top_k_frequent;
+
+    #[test]
+    fn test_case_1() {
+        assert_eq!(top_k_frequent(vec![1, 1, 1, 2, 2, 3], 2), vec![1, 2])
+    }
+
+    #[test]
+    fn test_case_2() {
+        assert_eq!(top_k_frequent(vec![1], 1), vec![1])
+    }
+
+    #[test]
+    fn test_case_3() {
+        assert_eq!(top_k_frequent(vec![-1, -1], 1), vec![-1])
+    }
+}
