158d62166b355c82a18d70fac98750bc51af6e6f
Tim OSullivan <44192816+invokermain@users.noreply.github.com>
2023-01-08 11:29:46 +0000

    feat (1588): added optimal solution

--- src/problem_1588.rs
+++ src/problem_1588.rs
@@ -25,40 +25,73 @@ fn sum_odd_length_subarrays(arr: Vec<i32>) -> i32 {
     sum
 }
 
+// This is the optimal solution, explained here: https://www.youtube.com/watch?v=J5IIH35EBVE
+// For each item in the array, the number of subarrays that include that item is
+// the combination of the number of arrays that start and end at that index.
+// The number that start at that index is (len_arr - idx).
+// The number that end at that index is (idx + 1).
+// Therefore the combination of the two is (len_arr - idx) * (idx + 1).
+// To get the number of odd length arrays we can do ((len_arr - idx) * (idx + 1) + 1) // 2.
+fn sum_odd_length_subarrays_optimal(arr: Vec<i32>) -> i32 {
+    let len_arr = arr.len();
+    let mut sum: i32 = 0;
+
+    for (idx, val) in arr.iter().enumerate() {
+        sum += val * (((idx + 1) * (len_arr - idx) + 1) / 2) as i32
+    }
+
+    sum
+}
+
 #[cfg(test)]
 mod tests {
-    use super::sum_odd_length_subarrays;
+    use super::*;
 
     #[test]
     fn test_case_1() {
-        assert_eq!(sum_odd_length_subarrays(vec![1, 4, 2, 5, 3]), 58)
+        let left = vec![1, 4, 2, 5, 3];
+        let right = 58;
+        assert_eq!(sum_odd_length_subarrays(left.clone()), right);
+        assert_eq!(sum_odd_length_subarrays_optimal(left), right);
     }
 
     #[test]
     fn test_case_2() {
-        assert_eq!(sum_odd_length_subarrays(vec![1, 2]), 3)
+        let left = vec![1, 2];
+        let right = 3;
+        assert_eq!(sum_odd_length_subarrays(left.clone()), right);
+        assert_eq!(sum_odd_length_subarrays_optimal(left), right);
     }
 
     #[test]
     fn test_case_3() {
-        assert_eq!(sum_odd_length_subarrays(vec![1, 2, 3]), 12)
+        let left = vec![1, 2, 3];
+        let right = 12;
+        assert_eq!(sum_odd_length_subarrays(left.clone()), right);
+        assert_eq!(sum_odd_length_subarrays_optimal(left), right);
     }
 
     #[test]
     fn test_case_4() {
-        assert_eq!(sum_odd_length_subarrays(vec![1, 2, 3, 4]), 25)
+        let left = vec![1, 2, 3, 4];
+        let right = 25;
+        assert_eq!(sum_odd_length_subarrays(left.clone()), right);
+        assert_eq!(sum_odd_length_subarrays_optimal(left), right);
     }
 
     #[test]
     fn test_case_5() {
-        assert_eq!(sum_odd_length_subarrays(vec![10, 11, 12]), 66)
+        let left = vec![10, 11, 12];
+        let right = 66;
+        assert_eq!(sum_odd_length_subarrays(left.clone()), right);
+        assert_eq!(sum_odd_length_subarrays_optimal(left), right);
     }
 
     #[test]
     fn test_case_6() {
-        assert_eq!(
-            sum_odd_length_subarrays(vec![6, 9, 14, 5, 3, 8, 7, 12, 13, 1]),
-            878
-        )
+        let left = vec![6, 9, 14, 5, 3, 8, 7, 12, 13, 1];
+        let right = 878;
+        assert_eq!(sum_odd_length_subarrays(left.clone()), right);
+        assert_eq!(sum_odd_length_subarrays_optimal(left), right);
     }
 }
